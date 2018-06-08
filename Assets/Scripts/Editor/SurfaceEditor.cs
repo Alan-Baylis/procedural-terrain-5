2f3a62369472ca105f46c2a3bdf72b97bd906d2f
Shiv Kumar <shivpkumar@gmail.com>
2018-06-08 12:13:01 -0700

    Update custom editor for Surface component

--- Assets/Scripts/Editor/SurfaceEditor.cs
+++ Assets/Scripts/Editor/SurfaceEditor.cs
@@ -3,12 +3,47 @@ using UnityEngine;
 
 [CustomEditor(typeof(Surface))]
 public class SurfaceEditor : Editor {
+    Surface surface;
+    SerializedProperty placeholder;
+    SerializedProperty tilePrefab;
+    SerializedProperty placeholderSize;
+    SerializedProperty tileSize;
+    SerializedProperty maxTileHeight;
+    SerializedProperty drawMode;
+    SerializedProperty smoothness;
+    SerializedProperty maxSteps;
+
+    void OnEnable() {
+        surface = (Surface)target;
+        placeholder = serializedObject.FindProperty("placeholder");
+        tilePrefab = serializedObject.FindProperty("tilePrefab");
+        placeholderSize = serializedObject.FindProperty("placeholderSize");
+        tileSize = serializedObject.FindProperty("tileSize");
+        maxTileHeight = serializedObject.FindProperty("maxTileHeight");
+        drawMode = serializedObject.FindProperty("drawMode");
+        smoothness = serializedObject.FindProperty("smoothness");
+        maxSteps = serializedObject.FindProperty("maxSteps");
+    }
 
     public override void OnInspectorGUI() {
-        DrawDefaultInspector();
-        
-        if (GUILayout.Button("Regenerate")) {
-            ((Surface)target).ResizeTiles();
+        serializedObject.Update();
+
+        EditorGUILayout.ObjectField(placeholder, typeof(Transform));
+        EditorGUILayout.ObjectField(tilePrefab, typeof(GameObject));
+        EditorGUILayout.PropertyField(placeholderSize);
+        EditorGUILayout.IntSlider(tileSize, 0, 20);
+        EditorGUILayout.IntSlider(maxTileHeight, 0, 10);
+        EditorGUILayout.PropertyField(drawMode);
+        EditorGUILayout.Slider(smoothness, 0, 10);
+
+        if (surface.drawMode == Surface.DrawMode.Steps) {
+            EditorGUILayout.IntSlider(maxSteps, 0, 10);
         }
+
+        if (GUILayout.Button("Regenerate")) {
+            surface.ResizeTiles();
+        }
+
+        serializedObject.ApplyModifiedProperties();
     }
 }
